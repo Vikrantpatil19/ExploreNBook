9c6ffff3e6c09c27ef6dc90ec3d8d5438a38b100
Vikrantpatil19 <pvikrant010@gmail.com>
2024-06-08 23:01:34 +0530

    Added the features section

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,6 +3,7 @@ import Navbar from '../../component/Navbar/Navbar'
 import Carousel from '../../component/Carousel/Carousel'
 import HeroAero from '../../component/Hero_Section/HeroAero/HeroAero'
 import HeroTrain from '../../component/Hero_Section/HeroTrain/HeroTrain'
+import Features from '../../component/Features_Section/Features'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Carousel/>
       <HeroAero/>
       <HeroTrain/>
+      <Features/>
     </div>
   )
 }
